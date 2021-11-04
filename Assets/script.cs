7358cc9a78a599b7e893d8f2162bd23998223d66
jesimielefraimdias <jesimiel.dias@gmail.com>
2021-11-04 03:21:38 -0300

    feat: object cylinder

---
+++ Assets/script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    private float speed = 2;
+    private float flag = -1;
+    private bool scale = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
