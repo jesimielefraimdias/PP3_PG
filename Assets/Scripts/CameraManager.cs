33ad80360103ffcc7f498e1135051adb6445869a
Lucas Henrique <lucash_araujo@outlook.com>
2021-11-25 20:06:55 -0300

    Adicionado a mudança de tamanho na perspectiva ortografica.

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -9,9 +9,14 @@ public class CameraManager : MonoBehaviour
     public GameObject cameraRelogio;
 
     public GameObject cameraObjetos;
+
+    private Camera[] cams;
+    private int tamanho;
     // Start is called before the first frame update
     void Start()
     {
+        cams = new Camera[Camera.allCamerasCount];
+        tamanho = Camera.GetAllCameras(cams);
         cameraPrincipal.SetActive(true);
         cameraRelogio.SetActive(false);
         cameraObjetos.SetActive(false);
@@ -49,6 +54,34 @@ public class CameraManager : MonoBehaviour
         {
             cameraObjetos.SetActive(false);
             cameraPrincipal.SetActive(true);
+        }
+        if(Input.GetKeyDown("w"))
+        {
+            for(int i = 0; i < tamanho; i++)
+            {
+                cams[i].orthographic = true;
+            }
+        }
+        else if(Input.GetKeyDown("s"))
+        {
+            for(int i = 0; i < tamanho; i++)
+            {
+                cams[i].orthographic = false;
+            }
+        }
+        else if(Input.GetKeyDown("up") && cams[0].orthographic == true && cams[0].orthographicSize > 1)
+        {
+            for(int i = 0; i < tamanho; i++)
+            {
+                cams[i].orthographicSize -= 1;
+            }
+        }
+        else if(Input.GetKeyDown("down") && cams[0].orthographic == true)
+        {
+            for(int i = 0; i < tamanho; i++)
+            {
+                cams[i].orthographicSize += 1;
+            }
         }
     }
 }
