5f23bd5acdc2f50b35ef593e794bec150d340f29
Rafael Toledo <rafaeltoledo@estudante.ufscar.br>
2021-11-27 14:13:41 -0300

    Movimentação do Cubo em Função do Cilindro.

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -4,32 +4,30 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
-    private float speed = 3;
-    private float flag = -1;
-    private bool scale = false;
+    private GameObject cilindro;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cilindro = GameObject.Find("Cilindro");
+        Debug.Log("Text: "+ cilindro.transform.position);
     }
 
     // Update is called once per frame
-        // Update is called once per frame
     void Update()
     {
-        float zIndex = transform.position[2];
-        
-        if(zIndex <= 3){
-            flag = 1;
-            transform.Rotate(90.0f, 0.0f, 0.0f, Space.World);
-            if(scale == false){
-                transform.localScale = transform.localScale*(float)1.5;
-                scale = true;
-            }
-        } else if (zIndex >= 25){
-            flag = -1;
-            transform.Rotate(90.0f, 0.0f, 0.0f, Space.World);
+        float diff = (cilindro.transform.position[2] - transform.position[2]);
+
+        if(diff > 9){
+            transform.Translate(0, 0, 1, Space.World);
+        } else if(diff < 9){
+            transform.Translate(0, 0, -1, Space.World);
         }
-        transform.Translate(0, 0, (flag*Time.deltaTime*speed), Space.World);
+
+        if(Input.GetKeyDown("4") && cilindro.transform.position[0] == -5 && transform.position[0] == -15){
+            transform.Translate(10, 0, 0, Space.World);
+        } else if(Input.GetKeyDown("6") && cilindro.transform.position[0] == -15 && transform.position[0] == -5){
+            transform.Translate(-10, 0, 0, Space.World);
+        }
     }
 }
